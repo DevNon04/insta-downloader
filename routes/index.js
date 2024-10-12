1912730039d30a67155595aba45a803c80b9aefa
NguyenCong04 <nguyentatcong2004@gmail.com>
2024-10-12 14:00:23 +0700

    update lolo

--- routes/index.js
+++ routes/index.js
@@ -180,6 +180,7 @@ router.get('/download', async function (req, res, next) {
                 headers: {
                     Accept: "application/json",
                     "Content-Type": "application/json",
+                    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3"
                 },
             })
 
